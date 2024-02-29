3972aa3549db3f87eac2c6607a638b59e404bbee
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2024-02-29 11:42:08 -0800

    addded movements
    fixed speed added oncoming cars

---
+++ BermudezPrototype-1P3/Assets/Scripts/Player Controler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input    
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);    
+    }
+}
